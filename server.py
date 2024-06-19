086e4b08b97a5df1bf8ce309e3575e4f2a1486fe
badarlarohannaidu <badarla.rohan@students.iiit.ac.in>
2024-06-19 11:51:28 +0530

    updated code

--- server.py
+++ server.py
@@ -10,11 +10,11 @@ setup_vehicle_database()
 
 @app.route('/')
 def root_dir():
-    return redirect('http://machinedpr.pythonanywhere.com/register')
+    return redirect('/register')
 
 @app.route('/register', methods=['GET', 'POST'])
 def registration():
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         designation = request.form['designation']
@@ -28,46 +28,46 @@ def registration():
     
     return render_template('register.html')
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
         userType = verify_sql(username, password)
-        if(userType == "Incorrect Credentials"):
+        if userType == "Incorrect Credentials":
             return render_template('login.html', msg="invalid")
         else:
-            url = "http://machinedpr.pythonanywhere.com/home/"+ userType + "/" + username
+            url = "http://machinedpr.pythonanywhere.com"+"/home/" + userType + "/" + username
             return redirect(url)
     return render_template('login.html')
 
-@app.route('http://machinedpr.pythonanywhere.com/home/<userType>/<username>', methods = ['GET'])
+@app.route('/home/<userType>/<username>', methods=['GET'])
 def home_page(userType, username):
     return render_template('home.html', username=username, userType=userType)
 
-@app.route('http://machinedpr.pythonanywhere.com/enter/<userType>/<username>', methods = ['GET', 'POST'])
+@app.route('/enter/<userType>/<username>', methods=['GET', 'POST'])
 def enter_page(userType, username):
-    if(request.method == 'GET'):
+    if request.method == 'GET':
         return render_template('index.html', username=username, userType=userType)
-    # Now its a post method
+    # Now it's a POST method
     # Here we need to store the data of the user in SQL
-    if(store_vehicle(request.form, userType, username)):
+    if store_vehicle(request.form, userType, username):
         return render_template('index.html', username=username, userType=userType)
     return render_template('index.html', username=username, userType=userType)
 
-@app.route('http://machinedpr.pythonanywhere.com/report/<userType>/<username>')
+@app.route('/report/<userType>/<username>')
 def report_page(userType, username):
     return render_template('getreport.html', userType=userType, username=username)
 
-@app.route('http://machinedpr.pythonanywhere.com/submit_dates/<userType>/<username>', methods=['GET'])
+@app.route('/submit_dates/<userType>/<username>', methods=['GET'])
 def submit_dates(userType, username):
     from_date = request.args.get('from-date')
     to_date = request.args.get('to-date')
     machine_name = request.args.get('machine-name')
 
     list_dict_data = fetch_vehicle_data(from_date, to_date, machine_name, userType, username)
-    return render_template('user_details.html', users = list_dict_data, userType=userType, username=username)
+    return render_template('user_details.html', users=list_dict_data, userType=userType, username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
